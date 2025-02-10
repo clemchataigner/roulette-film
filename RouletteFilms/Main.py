069d1a67ce4a705c2fe71f44bd52c5f2f5042aac
naiaMonde <naia.mondeteguy@gmail.com>
2025-02-10 13:34:48 +0100

    CA MARCHE OMG

--- RouletteFilms/Main.py
+++ RouletteFilms/Main.py
@@ -3,8 +3,8 @@ import random as rd
 # USERS
     # Récupération de WL et VU
     #Clément
-users = ['Clément', 'Naïa', 'Déclan', 'Lucie']
-
+users = ['Clément','Naïa','Déclan','Lucie']
+present = []
 for i in range(len(users)):
     user = users[i]  # Récupération du nom de l'utilisateur
 
@@ -30,42 +30,40 @@ for i in range(len(users)):
     
     if user == 'Clément' :
         clement = (vu_user, wl_user)
-        users.append(clement)
+        present.append(clement)
     elif user == 'Naïa' :
         naia = (vu_user, wl_user)
-        users.append(naia)
-    elif user == 'Déclan' :
+        present.append(naia)
+    elif user == 'Déclan':
         declan = (vu_user, wl_user)
-        users.append(declan)
+        present.append(declan)
     else :
         lucie = (vu_user, wl_user)
-        users.append(lucie)
+        present.append(lucie)
 
 ######################################################################################################################################################
 # initialistion 
 wl_commune = set()
 
 # trouve un utilisateur
-for source_wl in users:
-    # prend 1 film de la watchlist de source_wl
-    for film_wl in source_wl[1]:
+for source_wl in range(len(present)):
+    for film_wl in present[source_wl][1]:
         est_trouve = False
-        user_id = -1
+        user_id = 0
         # prend un utilisateur
         while est_trouve == False :
-            if user_id == len(users)-1 :
+            if user_id == len(present) :
                 break
-            user_id += 1
-            id_film_vu = -1        
+            id_film_vu = 0
             # parcours les films vu d'un user
             while est_trouve == False :
-                id_film_vu += 1
-                if film_wl == users[user_id][0][id_film_vu]:
+                if film_wl == present[user_id][0][id_film_vu] :
                     est_trouve = True
-                    break 
-                if id_film_vu == len(users[user_id][0])-1 :
                     break
-
+                if id_film_vu == len(present[user_id][0])-1 :
+                    break
+                id_film_vu += 1
+            user_id += 1         
         if est_trouve == False : # si on a pas trouvée le film_wl dans les films vu d'un user
             wl_commune.add(film_wl)
 
